565dff51a8061c45caaaa76932a1b66b337cb572
Esha <100985162+EshaAlii@users.noreply.github.com>
2023-04-26 17:15:32 -0700

    Update main.cpp
    Adjusted main file to accomodate the player header file

--- SpaceInvaders/BasicOpenGLProject/BasicOpenGLProject/BasicOpenGLProject/main.cpp
+++ SpaceInvaders/BasicOpenGLProject/BasicOpenGLProject/BasicOpenGLProject/main.cpp
@@ -1,65 +1,37 @@
 #include <GL/glut.h>
+#include "player.h"
+
+#define GL_SILENCE_DEPRECATION
 
 // Dimensions of the screen
 const float SCREEN_WIDTH = 480.0f;
 const float SCREEN_HEIGHT = 750.0f;
 
-// Position + Size of player
-float trianglePos[] = {SCREEN_WIDTH / 2.0f, 90.0f}; // position of the triangle (starts at the bottom)
-float triangleSize[] = {30.0f, 30.0f}; // size of the triangle
+// Create our player
+Player player;
 
 // Display Function
 void display()
 {
-    // Clear Screen
+    // Clear the screen
     glClear(GL_COLOR_BUFFER_BIT);
 
-    // Reset modelview matrix
+    // Load Identity Matrix
     glLoadIdentity();
 
     // Draw our player
-    glBegin(GL_TRIANGLES);
-    glColor3f(1.0f, 0.0f, 0.0f);
-    glVertex2f(trianglePos[0], trianglePos[1]);
-    glVertex2f(trianglePos[0] + triangleSize[0] / 2.0f, trianglePos[1] + triangleSize[1]);
-    glVertex2f(trianglePos[0] + triangleSize[0], trianglePos[1]);
-    glEnd();
+    player.display();
 
     // Swap Buffers
     glutSwapBuffers();
 
 }
 
-// Keyboard Function
-void keyboard(unsigned char key, int x, int y)
+// Keyboard Function for Player
+void playerKeyboard(unsigned char key, int x, int y)
 {
-    // For each key on our keyboard
-    switch (key)
-    {
-        // When the escape key is pressed
-        case 27:
-            // We exit the game
-            exit(0);
-            break;
-
-        // When a is pressed we move the triangle to the left
-        case 'a':
-            if (trianglePos[0] > 0)
-            {
-                trianglePos[0] -= 15.0f;
-            }
-            break;
-
-        // When d is pressed we move the triangle to the right
-        case 'd':
-            if (trianglePos[0] + triangleSize[0] < SCREEN_WIDTH)
-            {
-                trianglePos[0] += 15.0f;
-            }
-            break;
-    }
-
-    // Redraw the scene
+    player.playerLeft(key);
+    player.playerRight(key);
     glutPostRedisplay();
 }
 
@@ -74,14 +46,11 @@ int main(int argc, char** argv)
     // Set Window Size
     glutInitWindowSize(SCREEN_WIDTH, SCREEN_HEIGHT);
 
-     // Create window
-    glutCreateWindow("OpenGL Example");
+    // Create window
+    glutCreateWindow("SpaceInvaders");
 
     // Set Display callback function
     glutDisplayFunc(display);
-
-    // Set keyboard callback function
-    glutKeyboardFunc(keyboard);
 
     // Switch to projection mode
     glMatrixMode(GL_PROJECTION);
@@ -95,9 +64,13 @@ int main(int argc, char** argv)
     // Switch back to modelview mode
     glMatrixMode(GL_MODELVIEW);
 
+    // Set keyboard callback function
+    glutKeyboardFunc(playerKeyboard);
+
     // Enter main loop
     glutMainLoop();
 
     // End Program
     return 0;
 }
+
