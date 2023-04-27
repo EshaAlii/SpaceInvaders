f647cba2fb0594be5bee69beb66ac9844523d551
Esha <100985162+EshaAlii@users.noreply.github.com>
2023-04-26 17:09:35 -0700

    Update player.h
    Updated header file to include setter and getter function for player's movement. Additionally passed key callback in player left and right movement.

--- SpaceInvaders/BasicOpenGLProject/BasicOpenGLProject/BasicOpenGLProject/player.h
+++ SpaceInvaders/BasicOpenGLProject/BasicOpenGLProject/BasicOpenGLProject/player.h
@@ -23,14 +23,22 @@ class Player
     void display();
 
     // Function to move player left
-    void playerLeft();
+    void playerLeft(unsigned char key);
 
     // Function to move player right
-    void playerRight();
+    void playerRight(unsigned char key);
+
+    // Our "setter function" for our player's x position
+    void setPlayer(float x);
+
+    // Our "getter function" for our player's x position
+    float getX() const{return x;}
 
     // Function to have the player shoot a bullet
     void playerShoot();
 
+    // Destructor (for when player dies)
+    ~Player();
 
 };
 
