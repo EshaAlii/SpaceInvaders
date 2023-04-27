6c09f362596d5b77a473bc700c7647a99aa848ba
Esha <100985162+EshaAlii@users.noreply.github.com>
2023-04-26 17:04:43 -0700

    Update player.cpp
    Implemented a functional player.cpp file, player now moves left and right and has a display function with keyboard callback. Player is also smaller and position is centered in the middle of the screen along the x-axis. It's y-axis is stationary. Additionally, the player's width now cannot go beyond the screen and only its vertex touches the edges.

--- SpaceInvaders/BasicOpenGLProject/BasicOpenGLProject/BasicOpenGLProject/player.cpp
+++ SpaceInvaders/BasicOpenGLProject/BasicOpenGLProject/BasicOpenGLProject/player.cpp
@@ -1,14 +1,20 @@
 #include "player.h"
 
+#define SCREEN_WIDTH 480
+
+// Default constructor
 Player::Player()
 {
-    // Set the width
-    x = 2.0f;
+    // Set the Player's position
+    x = 240.0f;
     y = 90.0f;
-    width = 30.0 f;
-    height = 30.0 f;
+
+    // Set the dimensions of how the player shows up
+    width = 30.0f;
+    height = 30.0f;
 }
 
+// Display function to have the player show up
 void Player::display()
 {
     // Represent them with a triangle
@@ -20,15 +26,56 @@ void Player::display()
     glEnd();
 }
 
-void Player::playerLeft()
+// Function to move the player to the left of the screen
+void Player::playerLeft(unsigned char key)
 {
-    if (x > 0)
+    // If the user presses the a key
+    if (key == 'a')
     {
-        
+        // And if their current position is greater than the left edge of the screen
+        if (x > 1){
+
+            // The player will move left
+            x -= 15.0f;
+        }
     }
+
+    // Redisplay player
+    glutPostRedisplay();
 }
 
-void Player::playerRight()
+// Function to move the player to the right of the screen
+void Player::playerRight(unsigned char key)
 {
-    
+    // If the user presses the d key
+    if (key == 'd')
+    {
+        // And if the width of the player along with their current x position doesn't exceed the Screen Width
+        if (x + width + 1 < SCREEN_WIDTH)
+        {
+            // The player will move right
+            x += 15.0f;
+        }
+    }
+    // Redisplay Player
+    glutPostRedisplay();
+}
+
+//  Our "setter function" for our player to update their position
+void Player::setPlayer(float x, float y)
+{
+    // Point to current instance of their x position
+   this->x = x;
+}
+
+// Function to have the player shoot a bullet
+void Player::playerShoot()
+{
+    // Implement code
+}
+
+// Destructor of Player
+Player::~Player()
+{
+    // Implement player death + 3 lives of player here (if player get's hit thrice, they die)
 }
