890e967d346d435b53f4401f3b23a9f56d54cd0f
Esha <100985162+EshaAlii@users.noreply.github.com>
2023-05-01 00:32:14 -0700

    Update player.h
    finalized Player functionalities,

--- SpaceInvaders/BasicOpenGLProject/BasicOpenGLProject/BasicOpenGLProject/player.h
+++ SpaceInvaders/BasicOpenGLProject/BasicOpenGLProject/BasicOpenGLProject/player.h
@@ -1,13 +1,20 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 
+#include <iostream>
+
+#define GL_SILENCE_DEPRECATION
 #include <GL/glut.h>
+
+#include "bullet.h"
+#include <vector>
+
+using namespace std;
 
 // Create our Player
 class Player
 {
-    public:
-
+public:
     // Player Positions
     float x;
     float y;
@@ -16,11 +23,41 @@ class Player
     float width;
     float height;
 
+    // Player lives
+    int lives;
+
+    // Position for the lives
+    float lifex;
+    float lifey;
+
+    // Lives Dimensions
+    float lifewidth;
+    float lifeheight;
+
+    // Position for the score
+    float scorex;
+    float scorey;
+
+    int score;
+
+    int mag;
+
+    bool reviveCheck;
+
+    // Create a vector of bullets
+    vector<Bullet> bullets;
+
     // Constructor
     Player();
 
     // Display Function
     void display();
+
+    // Display Function for player's life
+    void displayLife();
+
+    // Display Function for player's score
+    void displayScore();
 
     // Function to move player left
     void playerLeft(unsigned char key);
@@ -32,14 +69,15 @@ class Player
     void setPlayer(float x);
 
     // Our "getter function" for our player's x position
-    float getX() const{return x;}
+    float getX() const { return x; }
 
     // Function to have the player shoot a bullet
-    void playerShoot();
+    void playerShoot(unsigned char key);
+
+    void revive(unsigned char key);
 
     // Destructor (for when player dies)
     ~Player();
-
 };
 
 #endif
