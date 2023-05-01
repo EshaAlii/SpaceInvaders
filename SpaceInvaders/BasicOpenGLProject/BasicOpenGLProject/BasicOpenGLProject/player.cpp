0b97ca7edc209fd6a54373509fa0783c587b0afc
Esha <100985162+EshaAlii@users.noreply.github.com>
2023-05-01 00:32:47 -0700

    Update player.cpp
    Finalized player.cpp

--- SpaceInvaders/BasicOpenGLProject/BasicOpenGLProject/BasicOpenGLProject/player.cpp
+++ SpaceInvaders/BasicOpenGLProject/BasicOpenGLProject/BasicOpenGLProject/player.cpp
@@ -1,5 +1,6 @@
 #include "player.h"
-#include "stb_image.h"
+#include <string>
+// #include "stb_image.h"
 
 #define STB_IMAGE_IMPLEMENTATION
 #define SCREEN_WIDTH 480
@@ -14,18 +15,85 @@ Player::Player()
     // Set the dimensions of how the player shows up
     width = 30.0f;
     height = 30.0f;
+
+    // Give our player three lives
+    lives = 3;
+
+    score = 0;
+
+    mag = 2;
+
+    reviveCheck = false;
 }
 
-// Display function to have the player show up
+// Function to display the player
 void Player::display()
 {
     // Represent them with a triangle
-    glBegin(GL_TRIANGLES);
-    glColor3f(1.0f, 0.0f, 0.0f);
-    glVertex2f(x,y);
-    glVertex2f(x + width / 2.0f, y + height);
-    glVertex2f(x + width, y);
-    glEnd();
+    if (lives != 0)
+    {
+        glBegin(GL_TRIANGLES);
+        glColor3f(0.0f, 1.0f, 0.0f);
+        glVertex2f(x, y);
+        glVertex2f(x + width / 2.0f, y + height);
+        glVertex2f(x + width, y);
+        glEnd();
+    }
+}
+
+// Function to display the life of the player
+void Player::displayLife()
+{
+    // Set the position of the lives
+    lifey = 20.0f;
+    lifex = 20.0f;
+
+    // Set the width and height of the lives
+    lifewidth = 15.0f;
+    lifeheight = 15.0f;
+
+    // For each life available
+    for (int i = 0; i < lives; i++)
+    {
+        // Represent them with a triangle
+        glBegin(GL_TRIANGLES);
+        glColor3f(0.0f, 1.0f, 0.0f);
+        glVertex2f(lifex, lifey);
+        glVertex2f(lifex + lifewidth / 2.0f, lifey + lifeheight);
+        glVertex2f(lifex + lifewidth, lifey);
+        glEnd();
+
+        // Additional spacing between each life
+        lifex = lifex + lifewidth + 15;
+    }
+}
+
+// Function to display the score of the player
+void Player::displayScore()
+{
+    // Set the position of the score
+    scorex = 375.0f;
+    scorey = 20.0f;
+
+    // Set the color of the score
+    glColor3f(1.0f, 1.0f, 1.0f);
+
+    // Set the font of the score
+    void *font = GLUT_BITMAP_HELVETICA_18;
+
+    // Convert the score to a string
+    string scoreStr = "Score: " + to_string(score);
+
+    // Loop through each character in the score string and display it
+    for (int i = 0; i < scoreStr.length(); i++)
+    {
+        glRasterPos2f(scorex, scorey);
+
+        glutBitmapCharacter(font, scoreStr[i]);
+
+        // Move the position of the score to the right for the next character
+        scorex = scorex + glutBitmapWidth(font, scoreStr[i]);
+    }
 }
 
 // Function to move the player to the left of the screen
@@ -35,7 +103,8 @@ void Player::playerLeft(unsigned char key)
     if (key == 'a')
     {
         // And if their current position is greater than the left edge of the screen
-        if (x > 1){
+        if (x > 1)
+        {
 
             // The player will move left
             x = x - 15.0f;
@@ -67,16 +136,33 @@ void Player::playerRight(unsigned char key)
 void Player::setPlayer(float x)
 {
     // Point to current instance of their x position
-   this->x = x;
+    this->x = x;
 }
 
 // Function to have the player shoot a bullet
 void Player::playerShoot(unsigned char key)
 {
-    if (key == ' ')
+
+    if (bullets.size() < mag)
     {
-        // Player shoots a bullet
+        if (key == ' ')
+        {
+            // Player shoots a bullet
+            bullets.push_back(Bullet(x, y));
+        }
     }
+}
+
+void Player::revive(unsigned char key)
+{
+    if (key == 'r')
+    {
+        lives = 3;
+        score = 0;
+        mag = 2;
+        reviveCheck = true;
+    }
+    glutPostRedisplay();
 }
 
 // Destructor of Player
